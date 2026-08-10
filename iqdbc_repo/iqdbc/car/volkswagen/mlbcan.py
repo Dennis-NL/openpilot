8d5114efa7800010b21a2385f0bab870f71ca445
Dennis <43215753+Dennis-NL@users.noreply.github.com>
2026-08-10 21:19:54 +0200

    Update mlbcan.py

--- iqdbc_repo/iqdbc/car/volkswagen/mlbcan.py
+++ iqdbc_repo/iqdbc/car/volkswagen/mlbcan.py
@@ -53,7 +53,7 @@ def create_acc_accel_control(packer, bus, accel, acc_control, stopping):
 
   acc_01_values = {
     "ACC_Status_ACC": acc_control,
-    "ACC_Sollbeschleunigung": accel if acc_enabled else 0,
+    "ACC_Sollbeschleunigung": accel if acc_enabled else 3.01,
     "ACC_zul_Regelabw_unten": 0.2 if acc_enabled else 0,
     "ACC_zul_Regelabw_oben": 0.2 if acc_enabled else 0,
     "ACC_neg_Sollbeschl_Grad": 4.0 if acc_enabled else 0,
